ede9c7898226bbbef8ccc43b615046ca17acee6e
Kerynn Davis <kerynn.davis@gmail.com>
2022-08-26 10:54:07 -0700

    Used shift method

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,4 @@ dogs = ["Charlie", "Kamee", "Ozzie", "Leo", "Mia"]
 dog_ages = [13, 9, 16, 7, 12]
 dog_prices = [150.00, 24.32, 52.78, 7.45, 10.15]
 own_dogs = [true, true, false, true, false]
+dogs.shift # The shift method will remove the first element of this array ("Charlie")
