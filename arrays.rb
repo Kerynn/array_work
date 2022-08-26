19fc0fd1b67d0d8d2d3a5ef6bb2aa6c637770c77
Kerynn Davis <kerynn.davis@gmail.com>
2022-08-26 10:59:57 -0700

    Demonstrated index positions

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,6 @@ dog_ages = [13, 9, 16, 7, 12]
 dog_prices = [150.00, 24.32, 52.78, 7.45, 10.15]
 own_dogs = [true, true, false, true, false]
 dogs.shift # The shift method will remove the first element of this array ("Charlie")
+dog_ages[2] # The element in the 2nd index position of this array is 16
+dogs[1] # The element in the index position of 1 of this array is "Kamee"
+dog_prices[4] # The element in the index position of 4 (which is also the last element) is 10.15
